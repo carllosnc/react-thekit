fada8d76ab09896dcc67a884714472ff3258113d
Carlos Costa <carllos.nc@gmail.com>
2022-04-06 18:30:14 -0300

    adding inter font 900

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@fontsource/inter/400.css'
 import '@fontsource/inter/700.css'
+import '@fontsource/inter/900.css'
 import '../styles/global.css'
 
 import { initReactI18next } from 'react-i18next'

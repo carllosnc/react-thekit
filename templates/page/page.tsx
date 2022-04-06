82f008e8d27797b53c0d216aaf6dde13174655ff
Carlos Costa <carllos.nc@gmail.com>
2022-04-06 18:57:40 -0300

    renaming title to React Thekit

--- templates/page/page.tsx
+++ templates/page/page.tsx
@@ -1,19 +1,18 @@
 import Head from 'next/head'
-
 import style from './page.module.scss'
 
 type PageProps = {
   children: any
 }
 
-export function Page(props: PageProps){
+export function Page(props: PageProps) {
   return (
     <div>
       <Head>
-        <title> CN Next Starter </title>
+        <title> React Thekit </title>
       </Head>
 
-      { props.children }
+      {props.children}
     </div>
   )
 }

37d9630fa4bf06bc40c2426443258a2f95c24a96
Carlos Costa <carllos.nc@gmail.com>
2022-06-05 20:59:42 -0300

    chore: update complete structure

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,10 @@
-import '@testing-library/jest-dom'
+// Optional: configure or set up a testing framework before each test.
+// If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
+
+// Used for __tests__/testing-library.js
+// Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect'
 
-// setup i18n
 import { initReactI18next } from 'react-i18next'
 import i18n from 'i18next'
 import { CONTENT } from './src/content'
--- src/services/index.ts
+++ src/services/index.ts
@@ -2,23 +2,26 @@ import Axios from 'axios'
 import useSWR from 'swr'
 
 export const HTTP = Axios.create({
-  'baseURL': 'https://baconipsum.com/',
-  'headers': {
-    'Content-Type': 'application/json;charset=utf-8'
-  }
+  baseURL: 'https://baconipsum.com/',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  },
 })
 
-export async function fetcher(url: string){
+export async function fetcher(url: string) {
   const response = await HTTP.get(url)
   return response.data
 }
 
-export function useLoremIpsum(){
-  const { data, error } = useSWR('/api/?type=all-meat&paras=2&start-with-lorem=1', fetcher)
+export function useLoremIpsum() {
+  const { data, error } = useSWR(
+    '/api/?type=all-meat&paras=2&start-with-lorem=1',
+    fetcher
+  )
 
   return {
     data: data,
     isLoading: !error && !data,
-    isError: error
+    isError: error,
   }
 }

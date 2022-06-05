e5c0a0f7e58fc7e51e236cc0a5e3993cd7652482
Carlos Costa <carllos.nc@gmail.com>
2022-06-05 03:20:14 -0300

    tests: update cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,9 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-  video: false,
-  screenshotOnRunFailure: false,
   e2e: {
-    baseUrl: 'http://localhost:3000',
-    specPattern: [
-      'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-      'components/**/*.cy.{js,jsx,ts,tsx}',
-    ],
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
-})
+});
--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,8 +1,5 @@
 describe('spec.cy.ts', () => {
   it('should visit', () => {
     cy.visit('/')
-    cy.contains('thekit')
   })
 })
-
-export {}

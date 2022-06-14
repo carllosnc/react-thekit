2cdb8ef3353c69312a70e132ba8acb0108a356fd
Carlos Costa <carllos.nc@gmail.com>
2022-06-14 08:09:53 -0300

    fix: some fixies

--- src/pages/carousel.tsx
+++ src/pages/carousel.tsx
@@ -25,22 +25,22 @@ export default function CarouselPage({ doc }) {
         items={[
           <MyImage
             key={111}
-            src="https://images.unsplash.com/photo-1506929562872-bb421503ef21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2568&q=80"
+            src="https://res.cloudinary.com/c4co/image/upload/v1655204172/thekit/images/02_zxxfjf.webp"
           />,
 
           <MyImage
             key={222}
-            src="https://images.unsplash.com/photo-1454391304352-2bf4678b1a7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2574&q=80"
+            src="https://res.cloudinary.com/c4co/image/upload/v1655204172/thekit/images/05_gm0bmx.webp"
           />,
 
           <MyImage
             key={333}
-            src="https://images.unsplash.com/photo-1515238152791-8216bfdf89a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80"
+            src="https://res.cloudinary.com/c4co/image/upload/v1655204172/thekit/images/04_hnipyy.webp"
           />,
 
           <MyImage
             key={333}
-            src="https://images.unsplash.com/photo-1473496169904-658ba7c44d8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            src="https://res.cloudinary.com/c4co/image/upload/v1655204172/thekit/images/03_vw6qtt.webp"
           />,
         ]}
       />
--- src/pages/image-expander.tsx
+++ src/pages/image-expander.tsx
@@ -6,39 +6,39 @@ import { key } from '@/utils'
 export default function ImageExpanderPage({ doc }) {
   const images = [
     {
-      src: 'https://images.unsplash.com/photo-1506929562872-bb421503ef21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2568&q=80',
-      width: 700,
-      height: 1000,
+      src: 'https://res.cloudinary.com/c4co/image/upload/v1655204172/thekit/images/02_zxxfjf.webp',
+      width: 793,
+      height: 596,
     },
 
     {
-      src: 'https://images.unsplash.com/photo-1454391304352-2bf4678b1a7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2574&q=80',
-      width: 600,
-      height: 600,
+      src: 'https://res.cloudinary.com/c4co/image/upload/v1655204172/thekit/images/05_gm0bmx.webp',
+      width: 793,
+      height: 1059,
     },
 
     {
-      src: 'https://images.unsplash.com/photo-1515238152791-8216bfdf89a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80',
-      width: 800,
-      height: 500,
+      src: 'https://res.cloudinary.com/c4co/image/upload/v1655204172/thekit/images/04_hnipyy.webp',
+      width: 792,
+      height: 1060,
     },
 
     {
-      src: 'https://images.unsplash.com/photo-1473496169904-658ba7c44d8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-      width: 600,
-      height: 600,
+      src: 'https://res.cloudinary.com/c4co/image/upload/v1655204172/thekit/images/03_vw6qtt.webp',
+      width: 794,
+      height: 530,
     },
 
     {
-      src: 'https://images.unsplash.com/photo-1535262412227-85541e910204?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80',
-      width: 600,
-      height: 400,
+      src: 'https://res.cloudinary.com/c4co/image/upload/v1655204172/thekit/images/01_kmi288.webp',
+      width: 794,
+      height: 597,
     },
 
     {
-      src: 'https://images.unsplash.com/photo-1475924156734-496f6cac6ec1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
-      width: 600,
-      height: 600,
+      src: 'https://res.cloudinary.com/c4co/image/upload/v1655204172/thekit/images/06_c1cu6c.webp',
+      width: 794,
+      height: 530,
     },
   ]
 

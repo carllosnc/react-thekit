4c3334bc66b4e24db5cb3164983526574f00a076
Carlos Costa <carllos.nc@gmail.com>
2022-09-07 21:53:47 -0300

    style: move component style to imports.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@fontsource/inter/400.css'
 import '@fontsource/inter/700.css'
 import '../styles/global.css'
+import '../styles/imports.css'
 
 import { initReactI18next } from 'react-i18next'
 import i18n from 'i18next'

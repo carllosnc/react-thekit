ad4149ffb47e4a9a2593ea928ba86cbc0cb700ad
Carlos Costa <carllos.nc@gmail.com>
2022-10-28 12:17:31 -0300

    add: <Selectable /> test

---
+++ src/components/Selectable/Selectable.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react'
+import { Selectable, SelectableItem } from './Selectable'
+
+describe('<Selectable /> component', () => {
+  test('check initial render', () => {
+    render(
+      <Selectable
+        start={1}
+        onSelect={data => {
+          console.log(data)
+        }}
+      >
+        <SelectableItem data={'foo'}>
+          <p> Foo </p>
+        </SelectableItem>
+
+        <SelectableItem data={'bar'}>
+          <p> Bar </p>
+        </SelectableItem>
+
+        <SelectableItem data={'span'}>
+          <p> Span </p>
+        </SelectableItem>
+      </Selectable>
+    )
+
+    expect(screen.getByTestId('selectable')).toBeInTheDocument()
+    expect(screen.getByTestId('selectable')).toBeVisible()
+
+    expect(screen.getAllByTestId('selectable-item').length).toBe(3)
+
+    expect(screen.getByTestId('selectable-mark')).toBeInTheDocument()
+    expect(screen.getByTestId('selectable-mark')).toBeVisible()
+  })
+})

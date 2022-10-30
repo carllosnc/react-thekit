66adf23807f544a4c8f8536515c381131b415f38
Carlos Costa <carllos.nc@gmail.com>
2022-10-30 01:14:09 -0300

    config: playwright

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,19 +1,11 @@
 /* eslint-disable testing-library/prefer-screen-queries */
+
 import { test, expect } from '@playwright/test'
 
-test('check homepage ', async ({ page }) => {
-  await page.goto('http://localhost:3000/')
+test('Home page', async ({ page }) => {
+  await page.goto('http://localhost:3000')
 
-  await expect(page).toHaveTitle(/React Thekit/)
+  const buttonLink = page.getByText('Button')
 
-  await expect(page.getByText('Button')).toBeVisible()
-})
-
-test('check button page', async ({ page }) => {
-  await page.goto('http://localhost:3000/')
-  const button = page.getByTestId('Button')
-
-  await button.click()
-
-  await expect(page).toHaveURL(/.*button/)
+  await expect(buttonLink).toBeVisible()
 })

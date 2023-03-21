04aa19c84380787c94b64b208b941b5a63ad99a0
Carlos Costa <carllos.nc@gmail.com>
2023-03-21 15:11:39 -0300

    adding support to foward ref

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, ReactNode } from 'react'
+import { ReactElement, forwardRef } from 'react'
 import { IconType } from 'react-icons'
 import { FiCheck } from 'react-icons/fi'
 
@@ -11,56 +11,72 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   description?: string
 }
 
-export function Input({
-  label,
-  error = false,
-  errorMessage,
-  icon,
-  success,
-  description,
-  ...props
-}: InputProps) {
-  const errorClass = error ? 'input__content--error' : ''
+const Input = forwardRef(
+  (
+    {
+      label,
+      error = false,
+      errorMessage,
+      icon,
+      success,
+      description,
+      ...props
+    }: InputProps,
+    ref: React.Ref<HTMLInputElement>
+  ) => {
+    const errorClass = error ? 'input__content--error' : ''
 
-  return (
-    <label data-testid="input" className="input">
-      <div data-testid="input-info" className="input__info">
-        {label && (
-          <p data-testid="input-label" className="input__label">
-            {label}
+    return (
+      <label data-testid="input" className="input">
+        <div data-testid="input-info" className="input__info">
+          {label && (
+            <p data-testid="input-label" className="input__label">
+              {label}
+            </p>
+          )}
+          {errorMessage && (
+            <p
+              data-testid="input-error-message"
+              className="input__error-message"
+            >
+              - {errorMessage}
+            </p>
+          )}
+        </div>
+
+        <div
+          data-testid="input-content"
+          className={`input__content ${errorClass}`}
+        >
+          {icon && (
+            <span data-testid="input-icon" className="input__icon">
+              {icon}
+            </span>
+          )}
+
+          <input
+            data-testid="input-field"
+            className="input__field"
+            {...props}
+            ref={ref}
+          />
+
+          {success && (
+            <span data-testid="input-success" className="input__success">
+              <FiCheck />
+            </span>
+          )}
+        </div>
+
+        {description && (
+          <p data-testid="input-description" className="input__description">
+            {description}
           </p>
         )}
-        {errorMessage && (
-          <p data-testid="input-error-message" className="input__error-message">
-            - {errorMessage}
-          </p>
-        )}
-      </div>
+      </label>
+    )
+  }
+)
 
-      <div
-        data-testid="input-content"
-        className={`input__content ${errorClass}`}
-      >
-        {icon && (
-          <span data-testid="input-icon" className="input__icon">
-            {icon}
-          </span>
-        )}
-
-        <input data-testid="input-field" className="input__field" {...props} />
-
-        {success && (
-          <span data-testid="input-success" className="input__success">
-            <FiCheck />
-          </span>
-        )}
-      </div>
-
-      {description && (
-        <p data-testid="input-description" className="input__description">
-          {description}
-        </p>
-      )}
-    </label>
-  )
-}
+Input.displayName = 'Input'
+export { Input }

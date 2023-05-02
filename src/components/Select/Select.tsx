c5ebf4524b943d9b0ff8b5e1f69f687feb381285
Carlos Costa <carllos.nc@gmail.com>
2023-05-02 12:05:10 -0300

    Adding support to fowardRef

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from 'react'
 import { FiChevronDown } from 'react-icons/fi'
 
 export type SelectItem = {
@@ -9,40 +10,67 @@ interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
   items: SelectItem[]
   placeholder?: string
   label?: string
+  error?: boolean
+  errorMessage?: string
 }
 
-export function Select({ placeholder, items, label, ...props }: SelectProps) {
-  return (
-    <div data-testid="select" className="select">
-      {label && (
-        <span data-testid="select-label" className="select__label">
-          {label}
-        </span>
-      )}
-      <div data-testid="select-content" className="select__content">
-        <select {...props} className="select__field">
-          {placeholder && (
-            <option data-testid="select-placeholder" hidden value="">
-              {placeholder}
-            </option>
+const Select = forwardRef(
+  (
+    { placeholder, items, label, error, errorMessage, ...props }: SelectProps,
+    ref: React.Ref<HTMLSelectElement>
+  ) => {
+    const errorClass = error ? 'select__field--error' : ''
+
+    return (
+      <div data-testid="select" className="select">
+        <div data-testid="select-info" className="select__info">
+          {label && (
+            <p data-testid="select-label" className="select__label">
+              {label}
+            </p>
           )}
+          {errorMessage && (
+            <p
+              data-testid="select-error-message"
+              className="select__error-message"
+            >
+              - {errorMessage}
+            </p>
+          )}
+        </div>
 
-          {items.map((item: SelectItem, index: number) => {
-            return (
-              <option
-                data-testid="select-option"
-                key={index + item.name}
-                className="select__option"
-                value={item.value}
-              >
-                {item.name}
+        <div data-testid="select-content" className="select__content">
+          <select
+            {...props}
+            className={`select__field ${errorClass}`}
+            ref={ref}
+          >
+            {placeholder && (
+              <option data-testid="select-placeholder" hidden value="">
+                {placeholder}
               </option>
-            )
-          })}
-        </select>
+            )}
 
-        <FiChevronDown data-testid="select-arrow" className="select__arrow" />
+            {items.map((item: SelectItem, index: number) => {
+              return (
+                <option
+                  data-testid="select-option"
+                  key={index + item.name}
+                  className="select__option"
+                  value={item.value}
+                >
+                  {item.name}
+                </option>
+              )
+            })}
+          </select>
+
+          <FiChevronDown data-testid="select-arrow" className="select__arrow" />
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
+)
+
+Select.displayName = 'Select'
+export { Select }
